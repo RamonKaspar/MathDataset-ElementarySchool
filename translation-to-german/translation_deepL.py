2e9bc22906d646bf8375407e6975cb0e92aeca96
Ramon Kaspar <kasparr@stundent.ethz.ch>
2024-05-08 17:10:38 +0200

    Script for translating the datasets with the free DeepL API

--- translation-to-german/translation_deepL.py
+++ translation-to-german/translation_deepL.py
@@ -8,20 +8,17 @@ load_dotenv()
 
 
 def translate_json_to_german(input_path, output_path):
-    df = pd.read_csv(input_path)
+    df = pd.read_csv(input_path, dtype={'question': str})   # Ensure that the question column is read as string
     auth_key = os.getenv("DEEPL_API_KEY")
     translator = deepl.Translator(auth_key)
     df['question'] = df['question'].apply(lambda x: translator.translate_text(x, target_lang="DE").text)
     df.to_csv(output_path, index=False)
     
 def csv_to_json(input_path, output_path):
-    df = pd.read_csv(input_path)
+    df = pd.read_csv(input_path, dtype={'question': str, 'answer': float})
     df.to_json(output_path, orient='records')
 
 if __name__ == "__main__":
-    
-    # NOTE: I think "\n" was not properly translated!!
-    
     # Translate to German
     input_path = "../data/I_Arithmetic/arithmetic_100.csv"
     output_path = "arithmetic_100_german.csv"
